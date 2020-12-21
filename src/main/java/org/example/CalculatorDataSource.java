e1d414d2d1d37449567215d96684c6e6ac80ef16
melkiimultic <melkii.multic@yandex.ru>
2020-12-22 01:57:58 +0300

    added try-catch -finally in  CalculatorDataSource and changed getConnection #2

--- src/main/java/org/example/CalculatorDataSource.java
+++ src/main/java/org/example/CalculatorDataSource.java
@@ -19,65 +19,34 @@ public class CalculatorDataSource {
         }
     }
 
-    private static Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(CON_STR);
-            connection.setAutoCommit(false);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private static Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(CON_STR);
+        connection.setAutoCommit(false);
         return connection;
     }
 
     static void createTable() {
-        Connection connection = getConnection();
-        try {
-            connection.beginRequest();
-            connection.createStatement().execute("CREATE TABLE IF NOT EXISTS Calculations " +
-                    "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    "expression TEXT NOT NULL)");
-            connection.commit();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                connection.rollback();
-                connection.close();
-            } catch (SQLException exception) {
-                exception.printStackTrace();
-            }
-        }
+        doWithConnection(conn -> conn.createStatement()
+                .execute("CREATE TABLE IF NOT EXISTS Calculations " +
+                        "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "expression TEXT NOT NULL)"));
     }
 
     static void saveExpression(String text) {
-        Connection conn = null;
-        try {
-            conn = getConnection();
-            conn.beginRequest();
-            Statement stmt = conn.createStatement();
-            ResultSet rSet = stmt.executeQuery(("SELECT COUNT(*) AS total FROM Calculations"));
-            int total = rSet.getInt("total");
-            if (total == 10) {
-                conn.createStatement().execute("DELETE FROM Calculations WHERE id=(SELECT MIN(id) FROM Calculations)");
-            }
-            stmt.close();
-            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Calculations (expression) VALUES (?)");
-            pstmt.setString(1, text);
-            pstmt.executeUpdate();
-            conn.commit();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                conn.rollback();
-                conn.close();
-            } catch (SQLException exception) {
-                exception.printStackTrace();
-            }
-        }
+        doWithConnection(
+                conn -> {
+                    Statement stmt = conn.createStatement();
+                    ResultSet rSet = stmt.executeQuery(("SELECT COUNT(*) AS total FROM Calculations"));
+                    int total = rSet.getInt("total");
+                    if (total == 10) {
+                        conn.createStatement().execute("DELETE FROM Calculations WHERE id=(SELECT MIN(id) FROM Calculations)");
+                    }
+                    stmt.close();
+                    PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Calculations (expression) VALUES (?)");
+                    pstmt.setString(1, text);
+                    pstmt.executeUpdate();
+                }
+        );
     }
 
     static List<String> selectAll() {
@@ -90,12 +59,13 @@ public class CalculatorDataSource {
             while (set.next()) {
                 expressions.add(set.getString("expression"));
             }
-
         } catch (SQLException exception) {
             throw new IllegalStateException(exception);
         } finally {
             try {
-                connection.close();
+                if (connection != null) {
+                    connection.close();
+                }
             } catch (SQLException exception) {
                 exception.printStackTrace();
             }
@@ -104,26 +74,35 @@ public class CalculatorDataSource {
         return expressions;
     }
 
+    private static void doWithConnection(SqlConsumer connectionConsumer) {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            connectionConsumer.consume(connection);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
 
-//    static void doWithConnection(SqlConsumer consumer) throws SQLException {
-//        try (Connection conn = DriverManager.getConnection(CON_STR)) {
-//            consumer.consume(conn);
-//        }
-//    }
-//
-//    static <T> T returnWithConnection(SqlFunction<T> func) throws SQLException {
-//        try (Connection conn = DriverManager.getConnection(CON_STR)) {
-//            return func.apply(conn);
-//        }
-//    }
+    @FunctionalInterface
+    interface SqlConsumer {
+        void consume(Connection conn) throws SQLException;
+    }
+
 }
-
-//@FunctionalInterface
-//interface SqlConsumer {
-//    void consume(Connection connection) throws SQLException;
-//}
-//
-//@FunctionalInterface
-//interface SqlFunction<T> {
-//    T apply(Connection connection) throws SQLException;
-//}
